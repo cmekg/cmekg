6e13eddf12eb346c99a98fb274a72297f0586a6d
cc <cc@com>
2026-06-05 19:55:08 +0800

    去掉VConsole

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 
 // 只在移动端或开发环境启用 vConsole
-import VConsole from 'vconsole'
-new VConsole()
+// import VConsole from 'vconsole'
+// new VConsole()
 
 const app = createApp(App)
 app.use(ElementPlus)
